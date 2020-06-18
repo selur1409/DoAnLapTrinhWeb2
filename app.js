5db2a478f242d023423cb2823411dc876adc6244
study <lngthv00@gmail.com>
2020-06-18 21:04:15 +0700

    create node modules with npm i express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello word!!!!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello expressjs');
+})
+
+const PORT = 3000;
+app.listen(PORT, function () {
+  console.log(`Server is running at http://localhost:${PORT}`);
+})
