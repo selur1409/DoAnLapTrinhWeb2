f36150cc9c29186252c9464ee60ed637e976b2b4
anhkhuong <01885393883h@gmail.com>
2020-06-19 10:56:30 +0700

    Create folder Models, public, views, layouts, file home.hbs, modify file app.js to set up view engine, install express-handlebars, render file index.html

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const exphbs = require('express-handlebars');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello expressjs');
+const hbs = exphbs.create({
+  defaultLayout: 'home',
+  extname: '.hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use('/public', express.static('public'))
+
+app.get('/index.html', function (req, res) {
+    res.render('index');
 })
 
 const PORT = 3000;
