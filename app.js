7771b89018deb823062da78b725170c5465f4923
hurts97 <vankiepbatphuc2910@gmail.com>
2020-06-23 20:28:24 +0700

    catch the error with express-async-errors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
 
 app.use(express.urlencoded({
@@ -24,8 +25,14 @@ app.get('/index.html', function (req, res) {
 const accountRoute = require('./route/account.route');
 app.use('/account', accountRoute);
 
+
 app.use(function (req, res) {
   res.render('404', { layout: false });
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).render('500', { layout: false });
 })
 
 const PORT = 3000;
