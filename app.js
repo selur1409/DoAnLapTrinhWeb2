fba960e819c59ab189f46d8f9dc98626a7a3d8ea
hurts97 <vankiepbatphuc2910@gmail.com>
2020-06-23 10:49:41 +0700

    404 page error

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs_sections = require('express-handlebars-sections');
 const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: 'home',
   extname: '.hbs',
-  helpers: {
-    section: hbs_sections()
-  }
 });
 
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-app.use(express.urlencoded({
-  extended: true
-}));
-
 app.use('/public', express.static('public'))
 
-// Trang chủ Home
-app.get('/', function (req, res) {
-  res.render('index');
-})
 app.get('/index.html', function (req, res) {
     res.render('index');
 })
 
-// route account
-const accountRoute = require('./route/account.route');
-app.use('/account', accountRoute);
+app.use(function (req, res) {
+  res.render('404', { layout: false });
+})
 
 const PORT = 3000;
 app.listen(PORT, function () {
