ffe80f0ca73578a731128c12aebd86a0fd0d535e
Hurts <vanthien.hurts97@gmail.com>
2020-07-27 23:19:42 +0700

    complete accept post

--- Models/editor.model.js
+++ Models/editor.model.js
@@ -1,35 +1,38 @@
 const db = require('../utils/db');
+const TBL_CATEGORIES = 'categories';
+const TBL_CATEGORIES_SUB = 'categories_sub';
+const TBL_TAG_POSTS = 'tag_posts';
+
 
 module.exports = {
     LoadTagOfPost: (idPost) => {
-        return db.load(`SELECT t.Id, t.Name,t.TagName FROM tag_posts tp, tags t, posts p WHERE t.Id=tp.IdTag AND tp.IdPost=p.Id AND p.Id=${idPost} and p.IsDelete=0 AND t.IsDelete=0`);
+        return db.load(`SELECT DISTINCT t.Id, t.Name,t.TagName FROM tag_posts tp, tags t, posts p WHERE t.Id=tp.IdTag AND tp.IdPost=p.Id AND p.Id=${idPost} and p.IsDelete=0 AND t.IsDelete=0`);
+    },
+    LoadAllTags: () => {
+        return db.load(`SELECT Id, TagName, Name FROM tags WHERE IsDelete=0`);
     },
     LoadCategoriesOfPost: (idPost) => {
-        return db.load(`SELECT DISTINCT catesub.IdCategoriesMain, p.IdCategories, catesub.Name FROM posts p, categories_sub catesub WHERE p.Id=${idPost} AND p.IdCategories=catesub.Id AND p.IsDelete=0 AND catesub.IsDelete=0`)
+        return db.load(`SELECT DISTINCT catesub.IdCategoriesMain, p.IdCategories, catesub.Name FROM posts p, ${TBL_CATEGORIES_SUB} catesub WHERE p.Id=${idPost} AND p.IdCategories=catesub.Id AND p.IsDelete=0 AND catesub.IsDelete=0`)
     },
 
     LoadCategoriesOfEditor: (IdAccount) => {
-        return db.load(`SELECT IdCategories FROM editoraccount WHERE IdAccount =${IdAccount} and IsDelete=0`);
+        return db.load(`SELECT edtacc.IdCategories, cate.Name FROM editoraccount edtacc, ${TBL_CATEGORIES} cate WHERE IdAccount =${IdAccount} and edtacc.IsDelete=0 AND cate.Id=edtacc.IdCategories ORDER BY edtacc.IdCategories ASC`);
     },
-    InsertTagPost: (value) => {
-        return db.insert(`INSERT INTO tag_posts(??, ??) VALUES ?`, value);
-    },
-
     LoadPostIsPending: (postStatus, idCategories) => {
         return db.load(`SELECT p.Id, p.Title,p.Content_Summary,p.DatePost,p.Avatar,p.IdCategories,p.IdStatus, cate.Name as'cateName',catesub.Name as 'cateSubName',
-         st.Name as'statusName' FROM posts p, categories cate, categories_sub cateSub,
+         st.Name as'statusName' FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub,
           status_posts st WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
            AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id`);
     },
     LoadPostIsAccept: (postStatus, idCategories) => {
         return db.load(`SELECT p.Id, p.Title,p.Content_Summary, p.DatePost,p.DatetimePost, p.Avatar,p.IdCategories,p.IdStatus, cate.Name as'cateName',catesub.Name as 'cateSubName',
-         st.Name as'statusName' FROM posts p, categories cate, categories_sub cateSub,
+         st.Name as'statusName' FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub,
           status_posts st WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
            AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id`);
     },
     LoadPostIsDeny: (postStatus, idCategories) => {
         return db.load(`SELECT p.Id, p.Title,p.Content_Summary, p.DatePost,fb.Note, p.Avatar,p.IdCategories,p.IdStatus, cate.Name as'cateName',catesub.Name as 'cateSubName',
-         st.Name as'statusName' FROM posts p, categories cate, categories_sub cateSub,
+         st.Name as'statusName' FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub,
           status_posts st, feedback fb WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
            AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id  AND fb.IdPost=p.Id AND fb.IsDelete=0`);
     },
@@ -40,7 +43,7 @@ module.exports = {
 
     LoadCategoriesById: (value) => {
         return db.load(`SELECT cate.Name as 'CateName', p.Id as 'idPost', cate.Id as 'idCategories' FROM 
-        posts p, categories cate, categories_sub catesub WHERE catesub.Id=p.IdCategories and p.IdCategories=${value} and 
+        posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} catesub WHERE catesub.Id=p.IdCategories and p.IdCategories=${value} and 
         cate.Id=catesub.IdCategoriesMain AND p.IsDelete=0 AND cate.IsDelete=0 AND catesub.IsDelete=0`);
     },
 
@@ -61,10 +64,54 @@ module.exports = {
         return db.insert(`INSERT INTO feedback(??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?)`, value);
     },
     LoadSubCategories: (idCategories) => {
-        return db.load(`SELECT Name, Id from categories_sub WHERE IdCategoriesMain=${idCategories} AND IsDelete=0`)
+        return db.load(`SELECT Name, Id from ${TBL_CATEGORIES_SUB} WHERE IdCategoriesMain=${idCategories} AND IsDelete=0`)
     },
     LoadTagsNotOfPost: (idPost) => {
         return db.load(`SELECT Name FROM tags WHERE TagName NOT IN(SELECT t.TagName FROM posts p, tag_posts tp, tags t WHERE p.Id=tp.IdPost AND p.Id=${idPost} AND tp.IdTag =t.Id)`);
+    },
+    LoadPostPending_Limit: (postStatus, idCategories, idCategoriesSub, limit, offset) => {
+        return db.load(`SELECT (ROW_NUMBER() OVER (ORDER BY p.Id)) as 'Stt', p.Id, p.Url, 
+        p.Title,p.Content_Summary,p.DatePost,p.Avatar,p.IdCategories,p.IdStatus, 
+        cate.Name as'cateName',catesub.Name as 'cateSubName',
+        st.Name as'statusName' 
+        FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub, status_posts st 
+        WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
+           AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id AND catesub.Id=${idCategoriesSub}
+        limit ${limit} offset ${offset}`);
+    },
+    LoadPostAccept_Limit: (postStatus, idCategories, idCategoriesSub, limit, offset) => {
+        return db.load(`SELECT (ROW_NUMBER() OVER (ORDER BY p.Id)) as 'Stt', p.Id, p.Url, p.Title,
+        p.Content_Summary, p.DatePost,p.DatetimePost, p.Avatar,p.IdCategories,p.IdStatus, 
+        cate.Name as'cateName',catesub.Name as 'cateSubName',
+        st.Name as'statusName' 
+        FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub, status_posts st 
+        WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
+           AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id AND catesub.Id=${idCategoriesSub}
+        limit ${limit} offset ${offset}`);
+    },
+    LoadPostDeny_Limit: (postStatus, idCategories, idCategoriesSub, limit, offset) => {
+        return db.load(`SELECT (ROW_NUMBER() OVER (ORDER BY p.Id)) as 'Stt', p.Id, p.Url,
+        p.Title,p.Content_Summary, p.DatePost,fb.Note, p.Avatar,p.IdCategories,p.IdStatus, 
+        cate.Name as'cateName',catesub.Name as 'cateSubName',
+        st.Name as'statusName'
+        FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub, status_posts st, feedback fb
+        WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
+           AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id AND catesub.Id=${idCategoriesSub} 
+           AND fb.IdPost=p.Id AND fb.IsDelete=0
+        limit ${limit} offset ${offset}`);
+    },
+    CountPost: (postStatus, idCategories, idCategoriesSub) => {
+        return db.load(`SELECT count(*) as'SoLuong' FROM posts p, ${TBL_CATEGORIES} cate, ${TBL_CATEGORIES_SUB} cateSub,
+         status_posts st WHERE p.IdCategories=cateSub.Id AND cateSub.IdCategoriesMain=cate.Id AND cate.Id=${idCategories}
+          AND p.IsDelete=0 AND p.IsDelete=0 AND p.IdStatus=${postStatus} AND p.IdStatus=st.Id AND catesub.Id=${idCategoriesSub}`)
+    },
+    DeleteTagsOfPost: (id) => {
+        const condition = {
+            IdPost: id
+        }
+        return db.del(TBL_TAG_POSTS, condition);
+    },
+    InsertTagsPost: (value) => {
+        return db.insert(`INSERT INTO tag_posts(??, ??) VALUES (?, ?)`, value);
     }
-
 }
--- Route/editor.route.js
+++ Route/editor.route.js
@@ -3,6 +3,8 @@ const editorModel = require('../models/editor.model');
 const moment = require('moment');
 const router = express.Router();
 const { restrict } = require('../middlewares/auth.mdw');
+const config = require('../config/default.json');
+const pageination = require('../js/pagination');
 
 function Authories(req, res, next) {
   const TypeAccount = res.locals.lcAuthUser.TypeAccount;
@@ -19,72 +21,153 @@ router.get('/', restrict, Authories, function (req, res) {
   });
 });
 
+
 router.get('/pending', restrict, Authories, async function (req, res) {
+  const listCate = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
+  const idCate = +req.query.cate || listCate[0].IdCategories;
+
+  const listCateSub = await editorModel.LoadSubCategories(idCate);
+  const idCateSub = +req.query.catesub || listCateSub[0].Id;
   const IdPostPending = 4;
-  const IsActivePending = true;
-  const idCategories = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
-  var list = [];
-  for (id = 0; id < idCategories.length; id++) {
-    const temp = await editorModel.LoadPostIsPending(IdPostPending, idCategories[id].IdCategories);
-    for (d of temp) {
-      d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
-      list.push(d);
+  const page = +req.query.page || 1;
+  if (page < 0) page = 1;
+  const offset = (page - 1) * config.pagination.limit;
+  const [list, total] = await Promise.all([
+    editorModel.LoadPostPending_Limit(IdPostPending, idCate, idCateSub, config.pagination.limit, offset),
+    editorModel.CountPost(IdPostPending, idCate, idCateSub)
+  ]);
+  for (c of listCate) {
+    if (c.IdCategories == idCate) {
+      c.isActiveCate = true;
     }
   }
+  for (cs of listCateSub) {
+    cs.idCate = idCate;
+    if (cs.Id == idCateSub) {
+      cs.isActiveCateSub = true;
+    }
+  }
+
+  const [nPages, page_items] = pageination.page(page, total[0].SoLuong);
+
+  for (d of list) {
+    d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
+  }
   res.render('vwEditor/listPostPending', {
-    IsActivePending: IsActivePending,
+    page_items,
+    IsActivePending: true,
     listPost: list,
+    prev_value: page - 1,
+    next_value: page + 1,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    listCate,
+    idCate,
+    listCateSub,
     empty: list.length === 0,
-    layout: 'homeeditor',
-    Status: IdPostPending
+    layout: 'homeeditor'
   });
 });
 
 router.get('/accepted', restrict, Authories, async function (req, res) {
+  const listCate = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
+  const idCate = +req.query.cate || listCate[0].IdCategories;
+
+  const listCateSub = await editorModel.LoadSubCategories(idCate);
+  const idCateSub = +req.query.catesub || listCateSub[0].Id;
   const IdPostAccepted = 1;
-  const IsActiveAccepted = true;
-  const idCategories = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
-  var list = [];
-  for (id = 0; id < idCategories.length; id++) {
-    const temp = await editorModel.LoadPostIsAccept(IdPostAccepted, idCategories[id].IdCategories);
-    for (d of temp) {
-      d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
-      d.DatetimePost= moment(d.DatetimePost).format('Do MMMM YYYY, HH:mm:ss');
-      list.push(d);
+  const page = +req.query.page || 1;
+  if (page < 0) page = 1;
+  const offset = (page - 1) * config.pagination.limit;
+  const [list, total] = await Promise.all([
+    editorModel.LoadPostAccept_Limit(IdPostAccepted, idCate, idCateSub, config.pagination.limit, offset),
+    editorModel.CountPost(IdPostAccepted, idCate, idCateSub)
+  ]);
+  for (c of listCate) {
+    if (c.IdCategories == idCate) {
+      c.isActiveCate = true;
     }
   }
+  for (cs of listCateSub) {
+    cs.idCate = idCate;
+    if (cs.Id == idCateSub) {
+      cs.isActiveCateSub = true;
+    }
+  }
+
+  const [nPages, page_items] = pageination.page(page, total[0].SoLuong);
+
+  for (d of list) {
+    d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
+  }
   res.render('vwEditor/listPostAccept', {
+    page_items,
+    IsActiveAccepted: true,
     listPost: list,
-    IsActiveAccepted: IsActiveAccepted,
+    prev_value: page - 1,
+    next_value: page + 1,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    listCate,
+    idCate,
+    listCateSub,
     empty: list.length === 0,
-    layout: 'homeeditor',
-    Status: IdPostAccepted
+    layout: 'homeeditor'
   });
 });
 
 router.get('/denied', restrict, Authories, async function (req, res) {
+  const listCate = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
+  const idCate = +req.query.cate || listCate[0].IdCategories;
+
+  const listCateSub = await editorModel.LoadSubCategories(idCate);
+  const idCateSub = +req.query.catesub || listCateSub[0].Id;
   const IdPostDenied = 3;
-  const IsActiveDenied = true;
-  const idCategories = await editorModel.LoadCategoriesOfEditor(res.locals.lcAuthUser.Id);
-  var list = [];
-  for (id = 0; id < idCategories.length; id++) {
-    const temp = await editorModel.LoadPostIsDeny(IdPostDenied, idCategories[id].IdCategories);
-    for (d of temp) {
-      d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
-      list.push(d);
+  const page = +req.query.page || 1;
+  if (page < 0) page = 1;
+  const offset = (page - 1) * config.pagination.limit;
+  const [list, total] = await Promise.all([
+    editorModel.LoadPostDeny_Limit(IdPostDenied, idCate, idCateSub, config.pagination.limit, offset),
+    editorModel.CountPost(IdPostDenied, idCate, idCateSub)
+  ]);
+  for (c of listCate) {
+    if (c.IdCategories == idCate) {
+      c.isActiveCate = true;
     }
   }
+  for (cs of listCateSub) {
+    cs.idCate = idCate;
+    if (cs.Id == idCateSub) {
+      cs.isActiveCateSub = true;
+    }
+  }
+
+  const [nPages, page_items] = pageination.page(page, total[0].SoLuong);
+
+  for (d of list) {
+    d.DatePost = moment(d.DatePost).format('Do MMMM YYYY, HH:mm:ss');
+  }
   res.render('vwEditor/listPostDeny', {
+    page_items,
+    IsActiveDenied: true,
     listPost: list,
-    IsActiveDenied: IsActiveDenied,
+    prev_value: page - 1,
+    next_value: page + 1,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    listCate,
+    idCate,
+    listCateSub,
     empty: list.length === 0,
-    layout: 'homeeditor',
-    Status: IdPostDenied
+    layout: 'homeeditor'
   });
 });
+
+
 router.get('/deny/:Id', restrict, Authories, async function (req, res) {
   const list = await editorModel.LoadInforPost(req.params.Id);
   const categories = await editorModel.LoadCategoriesById(list[0].IdCategories);
+
   res.render('vwEditor/deny', {
     categories: categories[0],
     denyPost: list[0],
@@ -96,24 +179,34 @@ router.get('/deny/:Id', restrict, Authories, async function (req, res) {
 router.get('/accept/:Id', restrict, Authories, async function (req, res) {
   const IdPost = req.params.Id;
   const categories = await editorModel.LoadCategoriesOfPost(IdPost);
-  const listCategoriesSub = await editorModel.LoadSubCategories(categories[0].IdCategoriesMain);
+  var listCategoriesSub = await editorModel.LoadSubCategories(categories[0].IdCategoriesMain);
   const inforOfPost = await editorModel.LoadSinglePost(IdPost);
   const author = await editorModel.LoadInforPost(IdPost);
-  let tempListCategoriesSub = [];
-  for (i of listCategoriesSub) {
-    if (i.Id != categories[0].IdCategories) {
-      tempListCategoriesSub.push(i);
+  for (c of listCategoriesSub) {
+    if (c.Id == categories[0].IdCategories) {
+      c.isActiveCategories = true;
+      break;
     }
   }
+
   const listTagOfPost = await editorModel.LoadTagOfPost(IdPost);
-  const listTags = await editorModel.LoadTagsNotOfPost(IdPost);
+  var templistTags = await editorModel.LoadAllTags();
+  for (t of templistTags) {
+    for (h of listTagOfPost) {
+      if (t.Id == h.Id) {
+        t.isActiveTags = true;
+        break;
+      }
+      else {
+        t.isActiveTags = false;
+      }
+    }
+  }
   res.render('vwEditor/accept', {
-    listCategoriesSub: tempListCategoriesSub,
-    listTagsOfPost: listTagOfPost,
-    listTags: listTags,
+    listCategoriesSub,
+    templistTags,
     author: author[0],
     inforOfPost: inforOfPost[0],
-    categoriesSubOfPost: categories[0],
     empty: listCategoriesSub.length === 0,
     layout: 'homeeditor'
   });
@@ -133,17 +226,36 @@ router.post('/denyPost', restrict, Authories, async function (req, res) {
   const result = await editorModel.InsertFeedbackPost(ValueOfFeedback);
   const entity = {
     Id: IdPost,
-    IdStatus: 3
+    IdStatus: IdPostDenied
   }
   const updateStatusPost = await editorModel.UpdateStatusPost(entity);
-  var success = false;
-  if (result != null) {
-    success = true;
-  }
+  
   res.redirect('/editor/pending');
-
 });
 
+router.post('/acceptPost', restrict, Authories, async function (req, res) {
+  const IsDelete = 0;
+  const IdPostAccept = 1;
+  const newListTags = req.body.tags;
+  const IdPost = req.body.IdPost;
+  const SubCate = req.body.subCate;
+  const ScheduleTime = req.body.Schedule;
+  const entity = {
+    Id: IdPost,
+    DatetimePost: ScheduleTime,
+    IdCategories: SubCate,
+    IdStatus: IdPostAccept
+  }
+  const deleteTagsPost=await editorModel.DeleteTagsOfPost(IdPost);
+  const listTagsOfPost = await editorModel.LoadTagOfPost(IdPost);
+  for(t of newListTags){
+    const value=['IdTag','IdPost',`${t}`,`${IdPost}`];
+    await editorModel.InsertTagsPost(value);
+  }
+  
+  const updateStatusPost = await editorModel.UpdateStatusPost(entity);
+  res.redirect('/editor/pending');
+});
 
 router.get('/reviewPost/:Id', restrict, Authories, async function (req, res) {
   const list = await editorModel.LoadSinglePost(req.params.Id);
